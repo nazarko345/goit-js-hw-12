f9d73bfa5e10bf9ebe3fcf7df0529b03edb1ff4f
Nazar <nazarijankosuk@gmail.com>
2025-03-25 01:08:42 +0200

    replace into async changes

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,9 +5,9 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const pixabayUrl = 'https://pixabay.com/api/';
 
-export const fetchImages = query => {
-  return axios
-    .get(pixabayUrl, {
+export const fetchImages = async query => {
+  try {
+    const getResponse = await axios.get(pixabayUrl, {
       params: {
         key: '49409853-9b66959f7621caabc2c0e0f94',
         q: query.trim(),
@@ -15,16 +15,16 @@ export const fetchImages = query => {
         orientation: 'horizontal',
         safesearch: true,
       },
-    })
-    .then(response => response.data.hits)
-    .catch(error => {
-      console.log(error);
-      iziToast.error({
-        title:
-          'Sorry, there are no images matching your search query. Please try again!',
-        position: 'topRight',
-        timeout: 5000,
-      });
-      throw error;
     });
+    return getResponse.data.hits;
+  } catch (error) {
+    console.log(error);
+    iziToast.error({
+      title:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
+      timeout: 5000,
+    });
+    throw error;
+  }
 };
